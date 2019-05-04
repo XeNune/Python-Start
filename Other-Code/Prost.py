2c0f43c9eea6b1f4ece1c89efb6f4b2d6ee2b759
Alexandr <50101331+XeNune@users.noreply.github.com>
2019-05-04 19:29:12 +0500

    Add files via upload

---
+++ Other-Code/Prost.py
@@ -0,0 +1,9 @@
+num = input("Как тебя зовут?: ")
+if num == "Alexandr":
+    print("Приветсвую")
+age = int (input("Сколько тебе лет?"))
+if age > 18:
+    print("Aga")
+elif age < 18:
+    print("Skoro")
+    
