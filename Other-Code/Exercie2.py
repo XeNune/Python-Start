e75451a0967129bd1722997fdb194a50b6dd2343
Alexandr <50101331+XeNune@users.noreply.github.com>
2019-05-07 22:46:05 +0500

    Update Exercie2.py

--- Other-Code/Exercie2.py
+++ Other-Code/Exercie2.py
@@ -2,7 +2,7 @@ num_1 = input ("First Num: ")
 num_2 = input ("Second Num: ")
 summ = int(num_1) + int(num_2)
 Word = input("Enter word: ")
-summ *= 228
+summ *= 228 # *= умножить что-то во сколько-то
 Word *= 2
 print (Word)
 print(summ)
